d810900bcb2d6e3ee95df9f337f02416e97de5a2
Sivanesh <sivanesh.call@gmail.com>
2019-03-14 11:54:58 +0530

    Added Links to sidenava

--- src/Components/SideNavContents.js
+++ src/Components/SideNavContents.js
@@ -4,37 +4,47 @@ import { Page, List, ListItem } from 'react-onsenui';
 class SideNavContents extends Component {
 	render() {
 		return (
-			<List>
+			<List id="side-nav">
 				<Page
 					renderFixed={() => (
 						<List
 							style={{ position: 'absolute', bottom: '0px', width: '100%' }}
 							renderHeader={() => <h4 id="side-nav-heading">Developers</h4>}
 						>
-							<ListItem ripple>
-								<div className="file-list">
-									<span>Sivanesh</span>
-								</div>
-							</ListItem>
-							<ListItem ripple>
-								<div className="file-list">
-									<span>Santhosh Kumar</span>
-								</div>
-							</ListItem>
+							{' '}
+							<a href="https://in.linkedin.com/in/sivanesh-shanmugam" target="_blank">
+								<ListItem ripple>
+									<div className="file-list">
+										<span>Sivanesh</span>
+									</div>
+								</ListItem>
+							</a>
+							<a href="https://in.linkedin.com/in/sivanesh-shanmugam" target="_blank">
+								<ListItem ripple>
+									<div className="file-list">
+										<span>Santhosh Kumar</span>
+									</div>
+								</ListItem>
+							</a>
 						</List>
 					)}
 				>
 					<List renderHeader={() => <h4 id="side-nav-heading">Settings</h4>}>
-						<ListItem ripple>
-							<div className="file-list">
-								<span>Documentation</span>
-							</div>
-						</ListItem>
-						<ListItem ripple>
-							<div className="file-list">
-								<span>Source Code</span>
-							</div>
-						</ListItem>
+						<a href="https://github.com/Sivanesh97/speech-to-js-react/blob/master/README.md">
+							<ListItem ripple>
+								<div className="file-list">
+									<span>Documentation</span>
+								</div>
+							</ListItem>
+						</a>
+
+						<a href="https://github.com/Sivanesh97/speech-to-js-react" target="_blank">
+							<ListItem ripple>
+								<div className="file-list">
+									<span>Source Code</span>
+								</div>
+							</ListItem>
+						</a>
 					</List>
 				</Page>
 			</List>
